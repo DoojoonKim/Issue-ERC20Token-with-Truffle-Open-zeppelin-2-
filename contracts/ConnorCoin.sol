5cf9dd9a005f7c862cec85c05808d6d1cff20126
Connor Kim(김두준) <43170570+DoojoonKim@users.noreply.github.com>
2019-02-24 12:22:35 +0900

    Add files via upload

---
+++ contracts/ConnorCoin.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
+
+contract ConnorCoin is ERC20, ERC20Detailed{
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20Detailed(_name, _symbol, _decimals) public{
+
+    }
+}
